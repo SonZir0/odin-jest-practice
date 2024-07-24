aae7973087bb3d26be19c63edefc861bca5caa88
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2024-07-24 18:04:05 +0400

    Complete assignment

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,64 @@
-// mock function to check if jest works
-export function sum(first, second) {
-    return first + second;
+export function capitalize(str) {
+    if (!str.length) return str;
+    return str[0].toUpperCase() + str.slice(1);
 }
-console.log('Printing from "index.js": 10 + 20 =', sum(10, 20));
+
+export function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+export const calculator = {
+    add: function (a, b) {
+        return a + b;
+    },
+
+    subtract: function (a, b) {
+        return a - b;
+    },
+
+    divide: function (a, b) {
+        return a / b;
+    },
+
+    multiply: function (a, b) {
+        return a * b;
+    },
+};
+
+export function caesarCipher(str, key) {
+    let cipher = '';
+    const messageChars = str.split('');
+
+    messageChars.forEach((character) => {
+        let charCode = character.charCodeAt();
+
+        if (charCode >= 65 && charCode <= 90)
+            cipher += shiftUpperCase(charCode, key);
+        else if (charCode >= 97 && charCode <= 122)
+            cipher += shiftLowerCase(charCode, key);
+        else cipher += character;
+    });
+    return cipher;
+}
+
+function shiftUpperCase(charCode, key) {
+    return String.fromCharCode(65 + ((charCode - 65 + key) % 26));
+}
+
+function shiftLowerCase(charCode, key) {
+    return String.fromCharCode(97 + ((charCode - 97 + key) % 26));
+}
+
+export function analyzeArray(arr) {
+    let min = Infinity;
+    let max = -Infinity;
+
+    let sum = arr.reduce((sum, current) => {
+        if (current < min) min = current;
+        if (current > max) max = current;
+        return (sum += current);
+    }, 0);
+
+    let average = sum / arr.length;
+    return { min, max, average, length: arr.length };
+}
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,63 @@
-import { sum } from '../src/js';
+import {
+    capitalize,
+    reverseString,
+    calculator,
+    caesarCipher,
+    analyzeArray,
+} from '../src/js';
 
-test('sum func: 1 + 2 = 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test('Capitalize: first letter function', () => {
+    expect(capitalize('something')).toBe('Something');
+    expect(capitalize('')).toBe('');
+});
+
+test('Capitalize: only the first letter', () => {
+    expect(capitalize('some string')).toBe('Some string');
+});
+
+test('Capitalize: check for empty strings', () => {
+    expect(capitalize('')).toBe('');
+});
+
+test('Reverse string', () => {
+    expect(reverseString('abcd')).toBe('dcba');
+});
+
+test('Calculator.add(): 20 + 3 = 23', () => {
+    expect(calculator.add(20, 3)).toBe(23);
+});
+
+test('Calculator.subtract(): 2 - 53 = -51', () => {
+    expect(calculator.subtract(2, 53)).toBe(-51);
+});
+
+test('Calculator.divide(): 10 / 2 = 5', () => {
+    expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test('Calculator.multiply(): 100 * 0.5 = 50', () => {
+    expect(calculator.multiply(100, 0.5)).toBe(50);
+});
+
+test('Caesar cipher', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc');
+});
+
+test('Caesar cipher: case preservation', () => {
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+});
+
+test('Caesar cipher: ignores non-alphabetical characters', () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+});
+
+test('Analyze array', () => {
+    const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+
+    expect(object).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6,
+    });
 });
